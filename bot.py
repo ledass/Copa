047d6d5bbcb48857dc1f7880d518b9d537bd02b9
ledass <nistalieocs@gmail.com>
2025-06-17 11:01:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,8 @@ class Bot(Client):
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
         await super().start()
-        await Media.ensure_indexes()
+        from database.ia_filterdb import ensure_indexes
+    await ensure_indexes()
         me = await self.get_me()
         temp.ME = me.id
         temp.U_NAME = me.username
