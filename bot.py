aa6a1a5a05284714de9c19f9fdea5c0e22b1d97f
ledass <nistalieocs@gmail.com>
2025-06-17 11:36:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,27 +36,27 @@ class Bot(Client):
         )
 
     async def start(self):
-    b_users, b_chats = await db.get_banned()
-    temp.BANNED_USERS = b_users
-    temp.BANNED_CHATS = b_chats
-    await super().start()
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
+        await super().start()
 
-    from database.ia_filterdb import ensure_indexes
-    await ensure_indexes()
+        from database.ia_filterdb import ensure_indexes
+        await ensure_indexes()
 
-    me = await self.get_me()
-    temp.ME = me.id
-    temp.U_NAME = me.username
-    temp.B_NAME = me.first_name
-    self.username = '@' + me.username
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
 
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
-    logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-    logging.info(LOG_STR)
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        logging.info(LOG_STR)
     
         
 
