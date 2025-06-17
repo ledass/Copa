11b73166f7a6ea98ef26e7a012210c8b65569c31
ledass <nistalieocs@gmail.com>
2025-06-17 18:20:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -17,7 +17,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_shortlink, get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_search_results
+from database.ia_filterdb import get_file_details, get_search_results, media_collection
 from database.filters_mdb import (
     del_all,
     find_filter,
